c7d20614b26c4ee08f7c8fd90102d97be5a856d0
uche2010199 <charles42miles@gmail.com>
2024-08-30 03:28:13 -0700

    changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-if(input =! " ")
+let input = document.querySelectorAll("input");
+// console.log(buttons);
+let sign = document.querySelector("#sign");
+
+// set click listen on all buttons
+for (let inputs of input) {
+  input.addEventListener("click", () => {
+    if (input.textContent == "") {
+      sign.toggleAttribute("diable");
+    }
+  });
+}
